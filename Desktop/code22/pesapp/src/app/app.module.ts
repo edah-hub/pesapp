2edb442cf2c37322beef982a3662ad6b0b3d24aa
edah-hub <edahchepngetich@gmail.com>
2022-11-08 21:30:46 +0300

    styling footer

--- Desktop/code22/pesapp/src/app/app.module.ts
+++ Desktop/code22/pesapp/src/app/app.module.ts
@@ -4,14 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    LoginComponent
+    LoginComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
