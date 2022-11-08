435f769122fa2c3f6ffbdbb84c3cef951cb298f3
edah-hub <edahchepngetich@gmail.com>
2022-11-08 21:57:30 +0300

    styling footer

--- Desktop/code22/pesapp/src/app/app.module.ts
+++ Desktop/code22/pesapp/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 
@@ -15,6 +16,7 @@ import { LoginComponent } from './login/login.component';
     HomeComponent,
     LoginComponent,
     FooterComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
